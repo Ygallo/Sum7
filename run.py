411aaae4ab28b972595f90817b3e2b24f71b2e98
Ygallo <yaninagallo@gmail.com>
2022-12-09 18:52:06 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+#import pyinputplus as pyip
+
+import pyfiglet
+
+sum7 = pyfiglet.figlet_format("Sum7", font = "bubble")
+print(sum7)
